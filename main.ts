96097468febb92ddccdbe0f8f7629511133dfe77
hottetsuo <91411166+hottetsuo@users.noreply.github.com>
2021-10-27 20:12:45 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+radio.setGroup(1)
+radio.sendValue("モード", 0)
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    radio.sendValue("ハンドル", Math.map(pins.analogReadPin(AnalogPin.P0), -1023, 1023, -100, 100))
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    radio.sendValue("アクセル", Math.map(pins.analogReadPin(AnalogPin.P1), -1023, 1023, -1, 1))
+})
